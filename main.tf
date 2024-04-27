6f4bd4365b4759efec788f3d8b4c170f07c0cb3e
ChaManRoRo <Thisishowwerave@gmail.com>
2024-04-27 15:13:51 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.25.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+project = "network-psemanning"
+region = "us-central1"
+credentials = "network-psemanning-100779004e66.json"
+
+}
+
+
+resource "google_storage_bucket" "bucket" {
+  name          = "quavobucketmayne"
+  location      = "us-central1"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
+
